b099ece77c5f77e93ac59e68b8d6df5501bee2bd
John R. Leeman <john@leemangeophysical.com>
2020-01-02 09:29:09 -0600

    Cleanup Skew-T notebook and use different variable names for main example and exercises.

--- pages/workshop/Skew_T/solutions/skewt_make_figure.py
+++ pages/workshop/Skew_T/solutions/skewt_make_figure.py
@@ -1,9 +1,9 @@
 # Make a figure
-fig = plt.figure(figsize=(10, 10))
+my_fig = plt.figure(figsize=(10, 10))
 
 # Make a SkewT object
-skew = SkewT(fig)
+my_skew = SkewT(my_fig)
 
 # Plot the temperature and dewpoint
-skew.plot(sounding['pressure'], sounding['temperature'], linewidth=2, color='tab:red')
-skew.plot(sounding['pressure'], sounding['dewpoint'], linewidth=2, color='tab:green')
+my_skew.plot(my_sounding['pressure'], my_sounding['temperature'], linewidth=2, color='tab:red')
+my_skew.plot(my_sounding['pressure'], my_sounding['dewpoint'], linewidth=2, color='tab:blue')
